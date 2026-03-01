b3f951cb944f395405c369021f4d1e4539ff799e
Beatrice Papi <G00461762@atu.ie>
2026-03-01 14:22:32 +0000

    Services Package

---
+++ src/main/java/atu/ie/ticketmaster/services/EventService.java
@@ -0,0 +1,24 @@
+package atu.ie.ticketmaster.services;
+
+import atu.ie.ticketmaster.model.Event;
+import atu.ie.ticketmaster.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event getEventById(Long id) {
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found"));
+    }
+}
