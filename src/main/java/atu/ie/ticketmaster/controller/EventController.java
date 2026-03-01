55d532465edcec07aeb252bbfc3ef2cceeb3a5e0
Beatrice Papi <G00461762@atu.ie>
2026-03-01 14:33:26 +0000

    Services Package

---
+++ src/main/java/atu/ie/ticketmaster/controller/EventController.java
@@ -0,0 +1,26 @@
+package atu.ie.ticketmaster.controller;
+
+import atu.ie.ticketmaster.model.Event;
+import atu.ie.ticketmaster.services.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/{id}")
+    public Event getEventDetails(@PathVariable Long id) {
+        return eventService.getEventById(id);
+    }
+}
---
+++ src/main/java/atu/ie/ticketmaster/controller/UserController.java
@@ -0,0 +1,21 @@
+package atu.ie.ticketmaster.controller;
+
+import atu.ie.ticketmaster.model.User;
+import atu.ie.ticketmaster.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody User user) {
+        return ResponseEntity.ok(userService.register(user));
+    }
+}
